bd18df19581a0423b0f5e8eaea51e37708f3b869
Prathamesh-Narayane <prathameshnarayane2802@gmail.com>
2023-11-03 09:28:15 +0530

    Container Config Done

---
+++ javaScript Practice/01-Variables.js
@@ -0,0 +1,17 @@
+const id = 12345;
+let accountEmail = "asd@gmail.com";
+var accountPassword = "123456";
+
+let tel;
+
+console.table([id, accountEmail, accountPassword, tel]);
+
+accountEmail = "dsa@gmail.com";
+accountPassword = "654321";
+
+console.table([id, accountEmail, accountPassword, tel]);
+
+// one line comments
+/*
+multiple line comments
+*/
---
+++ javaScript Practice/hello.js
@@ -0,0 +1,8 @@
+console.log("hello");
+console.log("hello");
+console.log("hello");
+console.log("hello");
+console.log("hello");
+console.log("hello");
+console.log("hell0o");
+console.log("hello");
